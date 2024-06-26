c85c79fde67f6a5d21c3b61d2dc12472448cbbd0
이민엽 <dyfldrpf9@naver.com>
2024-06-26 14:50:08 +0900

    start

---
+++ src/main/java/com/green/beadalyo/BeadalyoApplication.java
@@ -0,0 +1,13 @@
+package com.green.beadalyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeadalyoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeadalyoApplication.class, args);
+    }
+
+}
