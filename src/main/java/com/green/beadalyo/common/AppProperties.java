0b7bfb0139de522ba6aa3b47e2a922fdac4893ab
이민엽 <dyfldrpf9@naver.com>
2024-07-08 15:31:26 +0900

    이제 브랜치 팝시다~

---
+++ src/main/java/com/green/beadalyo/common/AppProperties.java
@@ -0,0 +1,27 @@
+package com.green.beadalyo.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@ConfigurationProperties(prefix = "app")
+public class AppProperties {
+    private final Jwt jwt = new Jwt();
+
+    @Getter
+    @Setter
+    public static class Jwt {
+        private String secret;
+        private String headerSchemaName;
+        private String tokenType;
+        private long accessTokenExpiry;
+        private long refreshTokenExpiry;
+        private int refreshTokenCookieMaxAge;
+
+        public void setRefreshTokenExpiry(long refreshTokenExpiry) {
+            this.refreshTokenExpiry = refreshTokenExpiry;
+            this.refreshTokenCookieMaxAge = (int) (refreshTokenExpiry * 0.001);
+        }
+    }
+}
