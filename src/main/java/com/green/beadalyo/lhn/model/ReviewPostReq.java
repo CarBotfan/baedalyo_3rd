294e2483b013a0fdbb3e763e67683c2d07bb3b97
Administrator <rkalgk183@gmail.com>
2024-07-10 17:58:51 +0900

    TLqkf

--- src/main/java/com/green/beadalyo/lhn/model/ReviewPostReq.java
+++ src/main/java/com/green/beadalyo/lhn/model/ReviewPostReq.java
@@ -1,5 +1,7 @@
 package com.green.beadalyo.lhn.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
@@ -7,16 +9,21 @@ import org.springframework.web.multipart.MultipartFile;
 @Getter
 @Setter
 public class ReviewPostReq {
-
+    @JsonIgnore
     private long reviewPk;
     private long doneOrderPk;
+    @JsonIgnore
     private long userPk;
     private long resPk;
     private String reviewContents;
     private int reviewRating;
+    @JsonIgnore
     private String reviewPics1;
+    @JsonIgnore
     private String reviewPics2;
+    @JsonIgnore
     private String reviewPics3;
+    @JsonIgnore
     private String reviewPics4;
 
 
