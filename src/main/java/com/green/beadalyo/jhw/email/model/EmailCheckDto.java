d122624536397e625d08fcc205d84817a5c421e7
Administrator <hyeongwoojeong@gmail.com>
2024-07-12 17:45:38 +0900

    07-12 이메일 추가중, 주소 mapper 문법오류 수정

--- src/main/java/com/green/beadalyo/jhw/email/model/EmailCheckDto.java
+++ src/main/java/com/green/beadalyo/jhw/email/model/EmailCheckDto.java
@@ -11,5 +11,6 @@ public class EmailCheckDto {
     @NotEmpty(message = "이메일을 입력해주세요.")
     @Email
     private String email;
+    @NotEmpty(message = "인증번호를 입력하세요.")
     private String authNum;
 }
