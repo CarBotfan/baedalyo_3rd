a6c642b4b8dcbfe56cbda3307775917d5d9a845f
이민엽 <dyfldrpf9@naver.com>
2024-07-29 10:38:39 +0900

    asdf

--- src/main/java/com/green/beadalyo/lmy/doneorder/entity/DoneOrder.java
+++ src/main/java/com/green/beadalyo/lmy/doneorder/entity/DoneOrder.java
@@ -7,6 +7,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Entity
 @Data
@@ -32,17 +34,21 @@ public class DoneOrder {
     @Column(length = 500)
     private String orderRequest;
 
+    @Column(length = 50)
     private String orderPhone;
 
+    @Column(length = 50)
     private String orderAddress;
 
     private String paymentMethod;
 
+    @ColumnDefault("0")
     private Integer orderMethod;
 
     private Integer doneOrderState;
 
     private String canceller;
 
+    @CreationTimestamp
     private String createdAt;
 }
