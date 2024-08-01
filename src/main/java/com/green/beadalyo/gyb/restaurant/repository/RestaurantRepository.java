7f7a4c8db9cb76671f27c472e46634e991ca25b7
Dong <poo0254@naver.com>
2024-08-01 14:50:02 +0900

    .

--- src/main/java/com/green/beadalyo/gyb/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/green/beadalyo/gyb/restaurant/repository/RestaurantRepository.java
@@ -3,6 +3,7 @@ package com.green.beadalyo.gyb.restaurant.repository;
 import com.green.beadalyo.gyb.model.Restaurant;
 import com.green.beadalyo.jhw.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -11,5 +12,6 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long>
 
     Optional<Restaurant> findTop1ByUser(Long user);
     Optional<Restaurant> findTop1BySeq(Long seq);
+
     Restaurant findRestaurantByUser(User user);
 }
