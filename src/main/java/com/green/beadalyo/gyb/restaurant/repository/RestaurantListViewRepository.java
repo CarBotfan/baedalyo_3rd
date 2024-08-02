29e34a4104be53ecf64512e11bd9891945b31bf7
GYB <gsb0708@gmail.com>
2024-08-02 16:05:36 +0900

    공영빈 08-02  - 거리별 정렬이 제대로 작동하지 않던 문제 수정

--- src/main/java/com/green/beadalyo/gyb/restaurant/repository/RestaurantListViewRepository.java
+++ src/main/java/com/green/beadalyo/gyb/restaurant/repository/RestaurantListViewRepository.java
@@ -26,18 +26,38 @@ public interface RestaurantListViewRepository extends JpaRepository<RestaurantLi
     @Query(value = "SELECT r.*, " +
             "(6371 * acos(cos(radians(:latitude)) * cos(radians(r.restaurantCoorX)) * cos(radians(r.restaurantCoorY) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(r.restaurantCoorX)))) AS distance " +
             "FROM restaurant_list_view r " +
-            "WHERE (:categoryId = 0 OR r.restaurantPk IN (SELECT a.restaurantPk FROM restaurant_list_view a JOIN cate_res_matching crm ON a.restaurantPk = crm.crm_res_pk WHERE crm.crm_cate_pk = :categoryId)) " +
+            "WHERE (r.restaurantPk IN (SELECT a.restaurantPk FROM restaurant_list_view a JOIN cate_res_matching crm ON a.restaurantPk = crm.crm_res_pk WHERE crm.crm_cate_pk = :categoryId)) " +
             "AND r.restaurantCoorX BETWEEN :xMin AND :xMax " +
             "AND r.restaurantCoorY BETWEEN :yMin AND :yMax " +
             "ORDER BY distance",
             countQuery = "SELECT count(*) " +
                     "FROM restaurant_list_view r " +
-                    "WHERE (:categoryId = 0 OR r.restaurantPk IN (SELECT a.restaurantPk FROM restaurant_list_view a JOIN cate_res_matching crm ON a.restaurantPk = crm.crm_res_pk WHERE crm.crm_cate_pk = :categoryId)) " +
+                    "WHERE (r.restaurantPk IN (SELECT a.restaurantPk FROM restaurant_list_view a JOIN cate_res_matching crm ON a.restaurantPk = crm.crm_res_pk WHERE crm.crm_cate_pk = :categoryId)) " +
                     "AND r.restaurantCoorX BETWEEN :xMin AND :xMax " +
                     "AND r.restaurantCoorY BETWEEN :yMin AND :yMax",
             nativeQuery = true)
     Page<RestaurantListView> findByCategoryIdAndCoordinatesSortedByDistance(@Param("categoryId") Long categoryId,
                                                                             @Param("xMin") BigDecimal xMin,
+                                                                            @Param("xMax") BigDecimal xMax,
+                                                                            @Param("yMin") BigDecimal yMin,
+                                                                            @Param("yMax") BigDecimal yMax,
+                                                                            @Param("latitude") BigDecimal latitude,
+                                                                            @Param("longitude") BigDecimal longitude,
+                                                                            Pageable pageable);
+
+    @Query(value = "SELECT r.*, " +
+            "(6371 * acos(cos(radians(:latitude)) * cos(radians(r.restaurantCoorX)) * cos(radians(r.restaurantCoorY) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(r.restaurantCoorX)))) AS distance " +
+            "FROM restaurant_list_view r " +
+            "WHERE r.restaurantCoorX BETWEEN :xMin AND :xMax " +
+            "AND r.restaurantCoorY BETWEEN :yMin AND :yMax " +
+            "ORDER BY distance",
+            countQuery = "SELECT count(*) " +
+                    "FROM restaurant_list_view r " +
+                    "WHERE r.restaurantPk IN (SELECT a.restaurantPk FROM restaurant_list_view a JOIN cate_res_matching crm ON a.restaurantPk = crm.crm_res_pk WHERE crm.crm_cate_pk = :categoryId) " +
+                    "AND r.restaurantCoorX BETWEEN :xMin AND :xMax " +
+                    "AND r.restaurantCoorY BETWEEN :yMin AND :yMax",
+            nativeQuery = true)
+    Page<RestaurantListView> findALLByCategoryIdAndCoordinatesSortedByDistance(@Param("xMin") BigDecimal xMin,
                                                                             @Param("xMax") BigDecimal xMax,
                                                                             @Param("yMin") BigDecimal yMin,
                                                                             @Param("yMax") BigDecimal yMax,
