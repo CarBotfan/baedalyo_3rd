28ae7a75653797bf9c21e83634d9f7d37f881dc1
Dong <poo0254@naver.com>
2024-08-08 16:57:06 +0900

    레스토랑 승인 부분 작업  (어드민)

--- src/main/java/com/green/beadalyo/gyb/restaurant/repository/RestaurantRepository.java
+++ src/main/java/com/green/beadalyo/gyb/restaurant/repository/RestaurantRepository.java
@@ -2,9 +2,11 @@ package com.green.beadalyo.gyb.restaurant.repository;
 
 import com.green.beadalyo.gyb.model.Restaurant;
 import com.green.beadalyo.jhw.user.entity.User;
+import com.green.beadalyo.kdh.accept.model.GetUnAcceptRestaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long>
@@ -15,4 +17,20 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, Long>
 
     Restaurant findRestaurantByUser(User user);
     Restaurant findRestaurantBySeq(Long seq);
+
+//    @Query(value = "SELECT res_pk AS resPk, " +
+//                        "res_user_pk AS resUserPk, " +
+//                        "res_name AS resName, " +
+//                        "res_regi_num AS resRegiNum, " +
+//                        "res_addr AS resAddr, " +
+//                        "res_description1 AS resDescription1, " +
+//                        "res_description2 AS resDescription2, " +
+//                        "created_at AS createdAt " +
+//                        "FROM restaurant " +
+//                        "WHERE res_state = 3",
+//                        nativeQuery = true)
+    List<Restaurant> findByState(Integer state);
+
+
 }
+
--- src/main/java/com/green/beadalyo/kdh/accept/AcceptService.java
+++ src/main/java/com/green/beadalyo/kdh/accept/AcceptService.java
@@ -1,15 +1,26 @@
-//package com.green.beadalyo.kdh.accept;
-//
-//import com.green.beadalyo.gyb.restaurant.repository.RestaurantRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class AcceptService {
-//    private final RestaurantRepository restaurantRepository;
-//
-//    public Integer acceptRestaurant(){
-//
-//    }
-//}
+package com.green.beadalyo.kdh.accept;
+
+import com.green.beadalyo.gyb.model.Restaurant;
+import com.green.beadalyo.gyb.restaurant.repository.RestaurantRepository;
+import com.green.beadalyo.kdh.accept.model.GetUnAcceptRestaurant;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AcceptService {
+    private final RestaurantRepository restaurantRepository;
+
+    public List<Restaurant> getUnAcceptRestaurant(){
+       return restaurantRepository.findByState(3);
+    }
+
+    public Integer acceptRestaurant(Long resPk){
+        Restaurant restaurant = restaurantRepository.getReferenceById(resPk);
+        restaurant.setState(2);
+        restaurantRepository.save(restaurant);
+        return 1;
+    }
+}
