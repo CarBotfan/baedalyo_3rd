159d83ca99d527f56358f7da1a1951b0da331c46
Administrator <hyeongwoojeong@gmail.com>
2024-08-09 15:06:04 +0900

    08-09

--- src/main/java/com/green/beadalyo/gyb/category/CategoryApiController.java
+++ src/main/java/com/green/beadalyo/gyb/category/CategoryApiController.java
@@ -13,10 +13,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.*;
 import java.util.List;
 
 @RestController
@@ -33,7 +35,7 @@ public class CategoryApiController
     private final AuthenticationFacade authenticationFacade;
     private final FileUtils fileUtils;
 
-    @PostMapping
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
     @Operation(summary = "카테고리 추가")
     @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = ResultDto.class)),
             description =
@@ -44,6 +46,62 @@ public class CategoryApiController
                     "<p> -4 : 파일 확장자 체크 실패(jpg, png, gif) </p>"
     )
     public Result putCategory(@RequestPart String str, @RequestPart MultipartFile file)
+    {
+//        User userGetRes = new User().Admin();
+        UserGetRes userGetRes = userService.getUserGetResByPk() ;
+        //유효성 검증
+        if (userGetRes == null)
+            return ResultError.builder().statusCode(-2).resultMsg("유저 정보가 일치하지 않습니다.").build();
+        //관리자 권한 검증
+        if (!userGetRes.getUserRole().equals("ROLE_ADMIN"))
+            return ResultError.builder().statusCode(-3).resultMsg("권한이 부족합니다.").build();
+
+        String filename = null ;
+        if (file != null && !file.isEmpty())
+        {
+            //파일 확장자 유효성 검증
+            try {
+                if (!fileUtils.checksumExt(List.of(Ext.GIF,Ext.PNG,Ext.JPG,Ext.JPEG) ,file))
+                    return ResultError.builder().statusCode(-4).resultMsg("파일은 jpg, gif, png 만 허용됩니다.").build();
+
+                filename = fileUtils.fileInput("category",file) ;
+            } catch (Exception e) {
+                log.error("An error occurred: ", e);
+                return ResultError.builder().build();
+            }
+
+        }
+
+        try {
+            service.InsertCategory(str, filename);
+        } catch (DuplicateKeyException e) {
+            try {
+                fileUtils.fileDelete(filename) ;
+            } catch (Exception e1) {log.error(e1.toString());}
+            return ResultError.builder().statusCode(-5).resultMsg("이미 만들어져있는 카테고리 입니다.").build() ;
+        } catch (Exception e) {
+            log.error("An error occurred: ", e);
+            try {
+                fileUtils.fileDelete(filename) ;
+            } catch (Exception e1) {log.error(e1.toString());}
+            return ResultError.builder().build();
+        }
+
+        return ResultDto.builder().build();
+
+    }
+
+    @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    @Operation(summary = "카테고리 추가")
+    @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = ResultDto.class)),
+            description =
+                    "<p> 1 : 정상 </p>" +
+                            "<p> -1 : 실패 </p>" +
+                            "<p> -2 : 로그인 정보 획득 실패 </p>" +
+                            "<p> -3 : 권한 부족 </p>" +
+                            "<p> -4 : 파일 확장자 체크 실패(jpg, png, gif) </p>"
+    )
+    public Result updateCategory(@RequestPart String str, @RequestPart MultipartFile file)
     {
 //        User userGetRes = new User().Admin();
         UserGetRes userGetRes = userService.getUserGetResByPk() ;
