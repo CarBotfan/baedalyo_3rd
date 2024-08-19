954f101c6b2ccaa0feb6f39bea30d3ab75874fcf
Dong <poo0254@naver.com>
2024-08-19 17:49:31 +0900

    스탯 일일 주문수 수정

--- src/main/java/com/green/beadalyo/lmy/doneorder/model/DailyOrderCountDto.java
+++ src/main/java/com/green/beadalyo/lmy/doneorder/model/DailyOrderCountDto.java
@@ -2,7 +2,7 @@ package com.green.beadalyo.lmy.doneorder.model;
 
 public interface DailyOrderCountDto {
     String getCreatedAt();
-    Long getDailyOrderCountDto();
+    Long getDailyOrderCount();
 
 
 }
